f73892cc0e93f41482cbc28a2a7aefb9ce5e2d87
Amev <45909140+Mama-Amev@users.noreply.github.com>
2026-01-31 07:49:39 -0500

    Added section that moves GIF, MP4, MP3, AND MOV files to directory.

--- JPG to PNG.ps1
+++ JPG to PNG.ps1
@@ -9,3 +9,22 @@ Get-ChildItem $SourceDir -Recurse -Include *.jpg,*.jpeg | ForEach-Object {
     $pngPath = Join-Path $OutDir ($_.BaseName + ".png")
     magick "$($_.FullName)" "$pngPath"
 }
+
+# ---- ADDITIONAL PART ----
+# Move GIF, MP4, MP3, MOV to OutDir
+Get-ChildItem $SourceDir -Recurse -Include *.gif,*.mp4,*.mp3,*.mov | ForEach-Object {
+    $destination = Join-Path $OutDir $_.Name
+
+    # Prevent overwrite
+    if (Test-Path $destination) {
+        $base = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+        $ext  = $_.Extension
+        $i = 1
+        do {
+            $destination = Join-Path $OutDir "$base`_$i$ext"
+            $i++
+        } while (Test-Path $destination)
+    }
+
+    Move-Item $_.FullName $destination
+}
